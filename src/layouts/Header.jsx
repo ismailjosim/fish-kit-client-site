0e60c611043a39ae1d73e9b3004989ce3c0f937f
ismail <ismailjosim99@gmail.com>
2023-10-23 01:55:47 +0600

    header drop down added

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import logo from '../assets/logo1.png'
 import { LuFish, LuFishOff } from 'react-icons/lu'
 
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import HeaderDropDown from './HeaderDropDown'
 
 const Header = () => {
@@ -11,6 +11,24 @@ const Header = () => {
     const toggleMenu = () => {
         setIsOpen(!isOpen)
     }
+
+    const classes =
+        'btn hover:btn-secondary rounded-md hover:text-white active:bg-primary active:text-white'
+
+    const menuItems = (
+        <>
+            <li>
+                <NavLink className={ classes } to='/'>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink className={ classes } to='/about'>
+                    about
+                </NavLink>
+            </li>
+        </>
+    )
 
     return (
         <nav className='relative bg-white shadow'>
@@ -40,21 +58,16 @@ const Header = () => {
                     {/* Mobile Menu open: "block", Menu closed: "hidden" */ }
                     <div
                         className={ `absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white ${ isOpen
-                            ? 'opacity-100 translate-x-0'
-                            : 'opacity-0 -translate-x-full'
+                                ? 'opacity-100 translate-x-0'
+                                : 'opacity-0 -translate-x-full'
                             } lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center` }
                     >
-                        <div className='flex flex-col lg:flex-row lg:items-center lg:mx-2'>
-                            <Link
-                                to={ '/' }
-                                className='px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-gray-100'
-                            >
-                                Home
-                            </Link>
+                        <div className='flex flex-col lg:flex-row lg:items-center lg:mx-2 list-none gap-5'>
+                            { menuItems }
                         </div>
-                        <div className='flex lg:flex-row md:flex-row flex-col lg:items-center mt-4 lg:mt-0'>
+                        <ul className='flex lg:flex-row md:flex-row flex-col lg:items-center mt-4 lg:mt-0 '>
                             <HeaderDropDown />
-                        </div>
+                        </ul>
                     </div>
                 </div>
             </div>
